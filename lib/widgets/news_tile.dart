70373660a28fd0374e71946f6134f866367a3d95
mohamed <mo.hatem1911@gmail.com>
2023-09-28 22:01:25 +0300

    Commit

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsTile extends StatelessWidget {
   const NewsTile({super.key, required this.articleModel});
 
   final ArticleModel articleModel;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          // Cashed network image
-          child: Image.network(
-            articleModel.image?? 'https://tse4.mm.bing.net/th?id=OIP.HxV79tFMPfBAIo0BBF-sOgHaEy&pid=Api&P=0&h=180',
-            height: 200,
-            width: double.infinity,
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Uri url = Uri.parse(articleModel.url);
+        launchUrl(url);
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            // Cashed network image
+            child: Image.network(
+              articleModel.image?? 'https://tse4.mm.bing.net/th?id=OIP.HxV79tFMPfBAIo0BBF-sOgHaEy&pid=Api&P=0&h=180',
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        Text(
-          articleModel.title,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(
-            color: Colors.black87,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
+          const SizedBox(
+            height: 12,
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        Text(
-          articleModel.subTitle?? '',
-          maxLines: 2,
-          style: const TextStyle(color: Colors.grey, fontSize: 14),
-        )
-      ],
+          Text(
+            articleModel.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            articleModel.subTitle?? '',
+            maxLines: 2,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
+          )
+        ],
+      ),
     );
   }
+
 }
